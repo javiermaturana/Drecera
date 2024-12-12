994bd72c95477a554fb580b677dfc7be8421b8b4
javiermaturana <jmaturana@opentrends.net>
2024-12-12 16:58:12 +0100

    Configuración segun entorno

--- core/db.php
+++ core/db.php
@@ -1,9 +1,21 @@
 <?php
-$servername = "localhost";
-$dbUser = getenv('DB_USER');
-$dbPass = getenv('DB_PASS');
-$dbName = getenv('DB_NAME');
+require_once __DIR__ . '/../env.php';
+
+if (ENVIRONMENT === 'local') {
+    require_once __DIR__ . '/../config.php'; // Configuración local
+}
+
+$servername = getenv('DB_SERVERNAME');
+    $dbUser = getenv('DB_USER');
+    $dbPass = getenv('DB_PASS');
+    $dbName = getenv('DB_NAME');
 
 // Crear conexión
 $conn = new mysqli($servername, $dbUser, $dbPass, $dbName);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}else{
+    echo "Conexión exitosa";
+}
 ?>
